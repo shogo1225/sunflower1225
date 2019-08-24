7201bc7dd7117499c9288eeefd4c19b837c8a327
shogo1225 <54409307+shogo1225@users.noreply.github.com>
2019-08-24 13:17:10 +0900

    Update Numer0n2.py

--- Numer0n2.py
+++ Numer0n2.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 def duplication_check(num):
     is_duble = ( len(num) == len(set(num)) )
     is_3num = ( len(num) == 3 )
@@ -30,34 +30,48 @@ def generate_number():
     return ''.join(random.sample("0123456789", 3))
 
 def play_game():
-    player_number = input_number('まず始めにあなたの番号を決めます')
+    data = []
+    player_deffence_number = input_number('まず始めにあなたの番号を決めます')
     random_number = str(random.randint(1,1000))
-    cpu_number = random_number
+    cpu_deffence_number = random_number
 
     while True:
 
         print('---------------------------------------------------')
-        player_number = input_number('コンピューターの３桁の数字を予想してください：')
-        eat, bite = eat_bite_check(player_number,cpu_number)
+        print('あなたが当てるです')
+        player_attacking_number = input_number('コンピューターの３桁の数字を予想してください：')
+        eat, bite = eat_bite_check(player_attacking_number,cpu_deffence_number)
         print(f'結果は[{eat}EAT-{bite}BITE]でした')
+
+        #データの入力
+        row=[player_attacking_number,eat,bite]
+        print(row)
+        
         if eat == 3:
             print('あなたの勝ちです')
-            return 
-        
-
+            return data
+    
         print('コンピューターがあなたの番号を予想します')
+        print('予測中')
+        time.sleep(2)
         cpu_number = generate_number()
         print('コンピューターは'+ str(cpu_number)+ 'と予想しました')
-        eat, bite = eat_bite_check(player_number,cpu_number)
+        eat, bite = eat_bite_check(player_deffence_number,cpu_number)
         print(f'結果は[{eat}EAT-{bite}BITE]でした')
+
+        #データの入力
+        row=[cpu_number,eat,bite]
+        print(row)
+
         if eat == 3:
             print('あなたの負けです')
-            return
+            return data
 
 if __name__ == '__main__':
     print('ゲームを開始します')
     play_game()
     print('ゲームを終了します')
+
 
 
 
